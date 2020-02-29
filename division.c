22822be32792310fcf5bfd88f5c6e65581048783
Ashutosh Bhatta <ashutoshb469@gmail.com>
2020-02-29 15:27:13 +0530

    Sign corrected

--- division.c
+++ division.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 int main() {    
 
-    int number1, number2, difference;
+    int number1, number2, quotient;
     
     printf("Enter two integers: ");
     scanf("%d %d", &number1, &number2);
 
     // calculating sum
-    difference = number1 - number2;      
+    quotient = number1 / number2;      
     
-    printf("%d + %d = %d", number1, number2, difference);
+    printf("%d + %d = %d", number1, number2, quotient);
     return 0;
 }
